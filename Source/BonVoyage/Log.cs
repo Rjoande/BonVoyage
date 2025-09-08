1dfa81609aff6eac4a5351dd8cc66b9847594f83
Lisias T <me@lisias.net>
2025-09-07 22:52:50 -0300

    Legal Mambo Jambo 2025 Edition.

--- Source/BonVoyage/Log.cs
+++ Source/BonVoyage/Log.cs
@@ -1,23 +1,6 @@
 ﻿/*
 	This file is part of Bon Voyage /L
-		© 2024 LisiasT : http://lisias.net <support@lisias.net>
-		© 2018-2024 Maja
-		© 2016-2018 RealGecko
-
-	Bon Voyage /L is licensed as follows:
-		* GPL 3.0 : https://www.gnu.org/licenses/gpl-3.0.txt
-
-	Bon Voyage /L is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-	You should have received a copy of the GNU General Public License 3.0
-	along with Bon Voyage /L. If not, see <https://www.gnu.org/licenses/>.
-
-*/
-/*
-	This file is part of Bon Voyage /L
-		© 2024 LisiasT : http://lisias.net <support@lisias.net>
+		© 2024-2025 LisiasT : http://lisias.net <support@lisias.net>
 
 	THIE FILE is licensed to you under:
 
@@ -26,7 +9,7 @@
 			copies of this license document, and changing it is allowed as long
 			as the name is changed.
 
-	THIE FILE is distributed in the hope that it will be useful,
+	THIS FILE is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 */
